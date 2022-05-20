b29505e184ab62d8085248658e251eecf7f30b5b
nongdingan <1146952400@qq.com>
2022-05-20 18:39:21 +0800

    完善用户授权的功能

--- routes/user/user.js
+++ routes/user/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const password = require('../../utils/password')
+// 后台管理员登录
+router.post('/login', function(req, res, next) {
+  const { username, password } = req.body
+  res.json({ code: 200, msg: '登录成功' })
 });
 
 module.exports = router;
