efccccb2a851ed3e1604717ae08c95c9bc103e7d
nongdingan <1146952400@qq.com>
2022-06-08 23:46:26 +0800

    修复了一些bug

--- utils/cors.js
+++ utils/cors.js
@@ -13,7 +13,9 @@ class Cors {
 }
 
 const cors_instance = new Cors({
-  origin: ['http://localhost:9528'],
+  // origin: ['http://localhost:9528'],
+  // origin: ['http://localhost:9528'],
+  origin: ['https://www.and2ui.cn', 'https://and2ui.cn'],
   optionsSuccessStatus: 200,
   allowedHeaders: ['Content-Type', 'authorization', 'username']
 });
